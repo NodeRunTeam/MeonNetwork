63acca37b01858fead6e364638bc0dc92d800428
Node Run <87899933+NodeRunTeam@users.noreply.github.com>
2023-04-05 23:03:07 +0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,14 +1,45 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/NodeRunTeam/NodeGuide/main/logo.sh
-chmod +x logo.sh && ./logo.sh
-sleep 7
-sudo apt-get update && sudo apt-get upgrade -y
-sleep 3
-wget https://raw.githubusercontent.com/NodeRunTeam/NodeGuide/main/docker.sh
-chmod +x docker.sh && ./docker.sh
-sleep 3
-mkdir muon-node
-cd muon-node
-curl -o docker-compose.yml https://raw.githubusercontent.com/muon-protocol/muon-node-js/testnet/docker-compose-pull.yml
-sleep 3
-docker compose up -d
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
+
+echo "---------------------------------------------------"
+echo -e "\e[32mШаг 1: Инициализация скрипта \e[0m" && sleep 2
+
+sudo apt install curl -y
+curl -s https://raw.githubusercontent.com/NodeRunTeam/NodeGuide/main/logo.sh | bash
+
+
+echo "---------------------------------------------------"
+echo -e "\e[32mВыберите действие. Введите на нужную цифру и подтвердите действие клавишей Enter\e[0m" && sleep 3
+echo -e ""
+echo "1 Установить ноду (Muone)"
+
+read doing
+
+case $doing in 
+  1) 
+  echo "---------------------------------------------------"
+  echo -e "\e[32mШаг 2: скачивание и установка ноды: \e[0m" && sleep 2
+
+  ssudo apt-get update && sudo apt-get upgrade -y
+  sleep 3
+  wget https://raw.githubusercontent.com/NodeRunTeam/NodeGuide/main/docker.sh
+  chmod +x docker.sh && ./docker.sh
+  sleep 3
+  mkdir muon-node
+  cd muon-node
+  curl -o docker-compose.yml https://raw.githubusercontent.com/muon-protocol/muon-node-js/testnet/docker-compose-pull.yml
+  sleep 3
+  docker compose up -d
+
+  echo "---------------------------------------------------"
+  echo -e "Установка ноды завершена."
+  echo "---------------------------------------------------"
+  sleep 1
+  echo "---------------------------------------------------"
+  echo -e "Проверьте свою ноду Alice: \e[32mhttps://alice.muon.net/\e[39m"
+  echo "---------------------------------------------------"
+  ;;
+esac
